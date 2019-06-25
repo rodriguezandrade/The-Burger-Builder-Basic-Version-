0f66cfc1358dff82df19a176e9094eba1a90bac4
Jonathan Andrade <1140117@unav.edu.mx>
2019-06-24 17:55:24 -0700

    minor changes

---
+++ theburguerbasic/src/components/Navigation/Toolbar/Toolbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const toolbar = (props) => {
+    return (
+        <header>
+            <div>Menu</div>
+            <div>Logo</div>
+            <nav>
+
+            </nav>
+        </header>
+    );
+};
+
+export default toolbar;
